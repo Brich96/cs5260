c9f300f74213a591d733ba3926afbcaff27fc075
benr96 <benstargate@gmail.com>
2022-10-26 03:58:18 +0000

    Current implementation of s3. bult class helper

---
+++ helper.py
@@ -0,0 +1,36 @@
+import boto3
+import json
+
+class BucketHelper():
+    
+    s3 = boto3.resource('s3')
+    s3_client = boto3.client('s3')
+    
+    def __init__(self):
+        pass
+    
+    def _getBucket(self, bucket_name):
+        return self.s3.Bucket(bucket_name)
+    
+    def listAllBuckets(self):
+        b = self.s3_client.list_buckets()
+        for item in b['Buckets']:
+            print(item['Name'])
+    
+    def deleteItem(self, bucket_name, key):
+        response = self.s3_client.delete_object(Bucket=bucket_name, Key=key)
+        return response
+    
+    # TODO: implement mass delete if needed
+    # response = s3_client.delete_objects(
+    #     Bucket=bucket_name,
+    #     Delete={"Objects": [{"Key": "text/test7.txt"}, {"Key": "text/test8.txt"}]},
+    # )
+    
+    def uploadItem(self, bucket_name, file_name, json_data):
+        s3obj = self.s3.Object(bucket_name, file_name)
+        response = s3obj.put(Body=(bytes(json.dumps(json_data).encode('UTF-8'))))
+        return response
+        
+    def getBucketItems(self, bucket_name):
+        return self._getBucket(bucket_name).objects.filter(MaxKeys=1)
--- listBuckets.py
+++ listBuckets.py
@@ -1,49 +1,54 @@
 import boto3
+import time
 import json
+from helper import BucketHelper
 
 
 
-s3 = boto3.resource('s3')
+
     
 session = boto3.Session()
 s3_client = session.client('s3')
-    
-def listClient():
-    b = s3_client.list_buckets()
-    for item in b['Buckets']:
-        print(item['Name'])
 
-# listClient()
-
-def deleteItem(bucket_name, key):
-    response = s3_client.delete_object(Bucket=bucket_name, Key=key)
-    print(response)
-    return response
-    
-    # response = s3_client.delete_objects(
-    #     Bucket=bucket_name,
-    #     Delete={"Objects": [{"Key": "text/test7.txt"}, {"Key": "text/test8.txt"}]},
-    # )
-    
-def uploadWidget(bucket_name, file_name, json_data):
-    s3obj = s3.Object(bucket_name, file_name)
-    
-    s3obj.put(Body=(bytes(json.dumps(json_data).encode('UTF-8'))))
+bucketHelper = BucketHelper()
+# bucketHelper.listAllBuckets()
     
 
-def getBucket():
 
-    bucket = s3.Bucket('usu-cs5260-waffle-requests')
-    for obj in bucket.objects.filter(MaxKeys=1).limit(1):
-        widget = json.loads(obj.get()['Body'].read())
-        print(widget)
-        print(obj.key)
-        if(widget['type'] == 'create'):
-            del widget['type']
-            del widget['requestId']
-            file_name = 'widgets/' + widget['owner'].lower().replace(" ", "-") + widget['widgetId']
-            uploadWidget('usu-cs5260-waffle-web', file_name, widget)
-        # deleteItem('usu-cs5260-waffle-requests', obj.key)
+
+    
+
+def handleRequests():
+
+    while True:
+    
+        bucketItems = bucketHelper.getBucketItems('usu-cs5260-waffle-requests')
+        
+        atLeastOneItem = False
+        for obj in bucketItems:
+            atLeastOneItem = True
+            widget = json.loads(obj.get()['Body'].read())
+            print(obj.key)
+            
+            if(widget['type'] == 'create'):
+                
+                # Remove uneeded keys
+                del widget['type']
+                del widget['requestId']
+                
+                # upload and delete request
+                file_name = 'widgets/' + widget['owner'].lower().replace(" ", "-") + widget['widgetId']
+                bucketHelper.uploadItem('usu-cs5260-waffle-web', file_name, widget)
+                bucketHelper.deleteItem('usu-cs5260-waffle-requests', obj.key)
+            
+            # TODO: implement change and delete
+            
+        if not atLeastOneItem:
+            print("No Requests Yet")
+            time.sleep(.2)
+            
+            
+            
         
     
-getBucket()
+handleRequests()
